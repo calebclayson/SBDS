5502fceb8458e3007560a83904b0dcce5269105a
taniz123456789 <jeditanis@gmail.com>
2020-04-30 16:01:37 -0600

    Main Menu Created

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        //create a new save file and start game at beginning
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
